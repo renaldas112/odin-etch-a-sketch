4edc3aa129c91c28eccc3407dfb485b0cff72afb
renaldas112 <renaldas112@gmail.com>
2022-09-03 16:02:29 +0300

    added font and changed few styles

--- grid.js
+++ grid.js
@@ -1,34 +1,31 @@
 //Sets important constants and variables
 
 const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
-
+let rows = document.getElementsByClassName("grid__container--grid-row");
+let cells = document.getElementsByClassName("grid__container--grid-cell");
 
 defaultGrid();
-//Creates a default grid sized 16x16 
+//Creates a default grid sized 16x16
 function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+  makeRows(16);
+  makeColumns(16);
 }
 
 //Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
-
-    //Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
+  //Creates rows
+  for (r = 0; r < rowNum; r++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "grid__container--grid-row";
+  }
+}
 
 //Creates columns
 function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+  for (i = 0; i < rows.length; i++) {
+    for (j = 0; j < cellNum; j++) {
+      let newCell = document.createElement("div");
+      rows[j].appendChild(newCell).className = "grid__container--grid-cell";
+    }
+  }
+}

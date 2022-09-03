03dd93e4ee73ac6e4a19673038add1164784b8a5
renaldas112 <renaldas112@gmail.com>
2022-09-03 21:09:59 +0300

    rainbow color added

--- script.js
+++ script.js
@@ -9,21 +9,41 @@
 const cellColor = document.querySelectorAll("div.change-color");
 
 //color mode input
-
 const inputColorMode = document.getElementById("colormode");
 
-//default color = #ff0000 (red)
-let theColor = "#ff0000";
+//default color = #fff (white)
+let theColor = "#fff";
 
-inputColorMode.addEventListener(
-  "input",
-  function () {
-    theColor = inputColorMode.value;
-  },
-);
-
-cellColor.forEach((changeColor) => {
-  changeColor.addEventListener("mouseover", (changeColor) =>  {
-    changeColor.target.style.backgroundColor = theColor;
-  });
+inputColorMode.addEventListener("input", function () {
+  theColor = inputColorMode.value;
 });
+
+const colorMode = document.querySelector(".color-mode");
+
+const toggleColorMode = () => {
+  cellColor.forEach((changeColor) => {
+    changeColor.addEventListener("mouseover", (changeColor) => {
+      changeColor.target.style.backgroundColor = theColor;
+    });
+  });
+};
+
+//color mode button
+colorMode.addEventListener("click", toggleColorMode);
+
+//rainbow mode
+
+const rainbowMode = document.querySelector(".rainbow-mode");
+
+//rainbow color
+
+const toggleRainbowMode = () => {
+    cellColor.forEach((changeColor) => {
+      changeColor.addEventListener("mouseover", (changeColor) => {
+        changeColor.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      });
+    });
+  };
+
+//rainbow mode button
+rainbowMode.addEventListener("click", toggleRainbowMode);
